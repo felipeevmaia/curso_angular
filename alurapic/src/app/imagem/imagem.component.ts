196c81761892d33996c80e6f92c7a18f8e69dac9
Felipe Maia <felipe.viveiros@gmail.com>
2019-06-27 20:05:07 -0300

    Adição do input de componente

--- alurapic/src/app/imagem/imagem.component.ts
+++ alurapic/src/app/imagem/imagem.component.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core"
+import { Component, Input } from "@angular/core"
 
 @Component({
     selector: 'ap-imagem',
     templateUrl: 'imagem.component.html'
 })
 export class ImagemComponent {
-  descriptionImagemMergulho = "mergulho"
+  @Input() descriptionImagemMergulho = "";
 
-  urlImagemMergulho = "https://natgeo.imgix.net/factsheets/thumbnails/yourshot-underwater-1869254.adapt.1900.1.jpg?auto=compress,format&w=1024&h=560&fit=crop"
+  @Input() urlImagemMergulho = "";
 
 }
