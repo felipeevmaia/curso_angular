a84f8878c59e968f9786b1215d02cf37d2aacb36
Felipe Maia <felipe.viveiros@gmail.com>
2019-06-27 07:01:08 -0300

    adição de código

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { ImagemComponent } from './imagem/imagem.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagemComponent
   ],
   imports: [
     BrowserModule
---
+++ alurapic/src/app/imagem/imagem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector: 'ap-imagem',
+    templateUrl: 'imagem.component.html'
+})
+export class ImagemComponent {
+  descriptionImagemMergulho = "mergulho"
+
+  urlImagemMergulho = "https://natgeo.imgix.net/factsheets/thumbnails/yourshot-underwater-1869254.adapt.1900.1.jpg?auto=compress,format&w=1024&h=560&fit=crop"
+
+}
